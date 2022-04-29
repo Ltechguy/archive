a1a6b59b7fdb2a98837d4b3d3a24eb880f54bd1b
PESD <102270504+JEMSHS@users.noreply.github.com>
2022-04-28 21:25:24 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,41 @@
+console.log('>> help')
+
+function validation() {
+    var headertext = document.getElementById('logo');
+    var textboxtext = document.getElementById('txtbox').value;
+  
+    if (textboxtext == 'bbc') {
+        window.open('https://JEMSHS.github.io/p-e-s-d-hangout-math/mathhangout.html');
+  }
+
+      if (textboxtext == 'keyyes') {
+      window.open('https://p-e-s-d.github.io/pesd-hangout-math/mathhangout.html');
+  }
+
+      if (textboxtext == "github") {
+      window.open('https://github.com/cens6r', '_blank')  
+  }
+  
+      if (textboxtext == 'lastfm') {
+      window.open('https://last.fm/user/void-star', '_blank');
+  }
+  
+      if (textboxtext == 'spotify') {
+      window.open('https://open.spotify.com/user/utqeto9r0v0qsgducht24jl3f', '_blank');
+  }
+
+      if (textboxtext == 'discord') {
+          window.open('discord://https://discord.com/users/581470718387159040', '_blank');
+      }
+
+  }
+  
+  function handle(e){
+      if(e.keyCode === 13){
+          e.preventDefault();
+          validation();
+          document.getElementById("txtbox").value = "";
+  
+      }
+  }
+  
